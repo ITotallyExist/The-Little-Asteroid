a5624e4f547548139aec33524ba2433c5fa24a70
unknown <gregoirebrougher@gmail.com>
2023-07-30 22:51:04 -0700

    added some printing stuff, still need to find the reasource id for the biome, ask in the discord

--- src/main/java/com/example/the_little_asteroid/Mixin/LevelRendererMixin.java
+++ src/main/java/com/example/the_little_asteroid/Mixin/LevelRendererMixin.java
@@ -36,7 +36,9 @@ public class LevelRendererMixin {
     
     private ResourceKey<Biome> spaceKey = ResourceKey.create(Registries.BIOME, new ResourceLocation("space"));
     private ResourceKey<Biome> spaceKey2 = ResourceKey.create(Registries.BIOME, new ResourceLocation(Asteroid.MOD_ID,"space"));
-
+    private ResourceKey<Biome> spaceKey3 = ResourceKey.create(Registries.BIOME, new ResourceLocation("plains"));
+    private ResourceKey<Biome> spaceKey4 = ResourceKey.create(Registries.BIOME, new ResourceLocation("biome.minecraft.plains"));
+    private ResourceKey<Biome> spaceKey5 = ResourceKey.create(Registries.BIOME, new ResourceLocation("biome.the_little_asteroid.space"));
 
     //remove clouds from space
     @Inject(method = "renderClouds", at = @At("HEAD"), cancellable = true)
@@ -44,10 +46,20 @@ public class LevelRendererMixin {
         BlockPos pos = new BlockPos((int) cameraX, (int) cameraY, (int) cameraZ);
 
         // System.out.println(((LevelRendererAccessor)((LevelRenderer)(Object)this)).getLevel().);
-
-
+        try {
+        // System.out.println(BuiltInRegistryKeys.isBuiltinBiome(spaceKey));
+        // System.out.println(BuiltInRegistryKeys.isBuiltinBiome(spaceKey2));
+        // System.out.println(BuiltInRegistryKeys.isBuiltinBiome(spaceKey3));
+        // System.out.println(BuiltInRegistryKeys.isBuiltinBiome(spaceKey4));
+        // System.out.println(BuiltInRegistryKeys.isBuiltinBiome(spaceKey5));
+        BuiltInRegistryKeys.biomeRegistryWrapper().listElementIds().forEach(y -> System.out.println(y.location()));//.toArray();
+            //after printing these out, i found that the biome that i created is just straight up fucking missing and now i want to cry
+        } catch (Exception e){
+            System.out.println(e);
+        }
         
         try{
+            
             if (((LevelRendererAccessor)((LevelRenderer)(Object)this)).getLevel().getBiome(pos).value() == BuiltInRegistryKeys.biomeRegistryWrapper().get(spaceKey).get().value()){
                 System.out.println("1");
                 info.cancel();
